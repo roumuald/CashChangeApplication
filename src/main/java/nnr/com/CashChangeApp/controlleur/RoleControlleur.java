77fb002a944317f4879fbc51b8e810908814d85c
Roumuald Noubiap Nomsi <roumualdnoubiap@gmail.com>
2023-11-25 05:42:35 +0100

    api controlleur utilisateur

--- src/main/java/nnr/com/CashChangeApp/controlleur/RoleControlleur.java
+++ src/main/java/nnr/com/CashChangeApp/controlleur/RoleControlleur.java
@@ -1,15 +1,14 @@
 package nnr.com.CashChangeApp.controlleur;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import nnr.com.CashChangeApp.entites.Role;
 import nnr.com.CashChangeApp.exception.CashChangeAppException;
+import nnr.com.CashChangeApp.exception.ErrorEntity;
 import nnr.com.CashChangeApp.services.InterfaceRoleService;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -20,21 +19,34 @@ public class RoleControlleur {
 
     @PostMapping("/saveRole")
     @ResponseStatus(HttpStatus.CREATED)
-    public Role saveRole(@RequestBody Role role){
-
-        return interfaceRoleService.saveRole(role);
+    public ResponseEntity saveRole(@RequestBody Role role){
+        try {
+            Role saveRole = interfaceRoleService.saveRole(role);
+            return new ResponseEntity<>(saveRole, HttpStatus.CREATED);
+        }catch (CashChangeAppException exception){
+           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorEntity("100", exception.getMessage()));
+        }
     }
-
     @GetMapping("/getAllRole")
     @ResponseStatus(HttpStatus.OK)
-    public List<Role> getAllRole(){
-        return interfaceRoleService.getAllRole();
+    public ResponseEntity getAllRole(){
+        try {
+            List<Role> roles=interfaceRoleService.getAllRole();
+            return new ResponseEntity<>(roles, HttpStatus.OK);
+        }catch (CashChangeAppException exception) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ErrorEntity("100", exception.getMessage()));
+        }
     }
 
     @PutMapping("/updateRole/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Role updateRole(@PathVariable Long id, @RequestBody Role newRole){
-        return interfaceRoleService.updateRole(id, newRole);
+    public ResponseEntity updateRole(@PathVariable Long id, @RequestBody Role newRole){
+        try {
+            Role role= interfaceRoleService.updateRole(id, newRole);
+            return new ResponseEntity(role, HttpStatus.OK);
+        }catch (CashChangeAppException exception){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ErrorEntity("120", exception.getMessage()));
+        }
     }
 
     @DeleteMapping("/deleteRole/{id}")
@@ -45,10 +57,8 @@ public class RoleControlleur {
             return new ResponseEntity<>("role supprimer avec succes", HttpStatus.OK);
         }catch (CashChangeAppException exception){
             return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-        } catch (DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             return new ResponseEntity<>("Impossible de supprimer le rôle en raison de contraintes de clé étrangère", HttpStatus.BAD_REQUEST);
-        } catch (Exception e){
-            return new ResponseEntity<>("une erreur est survenu", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
--- src/main/java/nnr/com/CashChangeApp/controlleur/UtilisateurControlleur.java
+++ src/main/java/nnr/com/CashChangeApp/controlleur/UtilisateurControlleur.java
@@ -4,14 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nnr.com.CashChangeApp.dto.AuthenticationDto;
 import nnr.com.CashChangeApp.entites.Utilisateur;
+import nnr.com.CashChangeApp.exception.CashChangeAppException;
+import nnr.com.CashChangeApp.exception.ErrorEntity;
 import nnr.com.CashChangeApp.services.InterfaceUtilisateurService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -25,13 +29,23 @@ public class UtilisateurControlleur {
 
     @PostMapping("/inscription/{idRole}")
     @ResponseStatus(HttpStatus.CREATED)
-    public void inscription(@RequestBody Utilisateur utilisateur, @PathVariable Long idRole){
-        interfaceUtilisateurService.inscription(utilisateur, idRole);
+    public ResponseEntity inscription(@RequestBody Utilisateur utilisateur, @PathVariable Long idRole){
+        try {
+            interfaceUtilisateurService.inscription(utilisateur, idRole);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new CashChangeAppException("Utilisateur cre avec success"));
+        }catch (CashChangeAppException exception){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorEntity("120", exception.getMessage()));
+        }
     }
     @PostMapping("/activation")
     @ResponseStatus(HttpStatus.CREATED)
-    public void activation(@RequestBody Map<String, String> activation){
-        interfaceUtilisateurService.activation(activation);
+    public ResponseEntity activation(@RequestBody Map<String, String> activation){
+        try {
+            interfaceUtilisateurService.activation(activation);
+            return ResponseEntity.status(HttpStatus.OK).body(new CashChangeAppException("code d'activation envoye"));
+        }catch (CashChangeAppException exception){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorEntity("502", exception.getMessage()));
+        }
     }
     @PostMapping("/connexion")
     public Map<String, String> connexion(@RequestBody AuthenticationDto authenticationDto){
@@ -41,5 +55,67 @@ public class UtilisateurControlleur {
         log.info("le resultat est ", auth.isAuthenticated());
         return null;
     }
-
+    @DeleteMapping("/deleteUser/{id}")
+    public ResponseEntity deteleUser(@PathVariable Long id){
+        try {
+            this.interfaceUtilisateurService.deteleUser(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new CashChangeAppException("Utilisateur supprime avec success"));
+        }catch (CashChangeAppException exception){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorEntity("502", exception.getMessage()));
+        }
+    }
+    @PutMapping("/updateUser/{id}")
+    public ResponseEntity updateUser(@PathVariable Long id, @RequestBody Utilisateur newUser){
+        try {
+            Utilisateur utilisateur=this.interfaceUtilisateurService.updateUser(id, newUser);
+            return ResponseEntity.ok(utilisateur);
+        }catch (CashChangeAppException exception){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ErrorEntity("500", exception.getMessage()));
+        }
+    }
+    @GetMapping("/getAllUser")
+    public ResponseEntity getAllUser(){
+        try {
+            List<Utilisateur> utilisateurs = this.interfaceUtilisateurService.getAllUser();
+            return ResponseEntity.ok(utilisateurs);
+        }catch (CashChangeAppException exception){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ErrorEntity("508", exception.getMessage()));
+        }
+    }
+    @GetMapping("/getOneUser/{id}")
+    public ResponseEntity getOneUser(@PathVariable Long id){
+        try {
+            Utilisateur utilisateur = this.interfaceUtilisateurService.getOneUser(id);
+            return ResponseEntity.ok(utilisateur);
+        }catch (CashChangeAppException exception){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ErrorEntity("510", exception.getMessage()));
+        }
+    }
+    @PostMapping("/addRoleToUser/{idUser}/{idRole}")
+    public ResponseEntity addRoleToUser(@PathVariable Long idUser, @PathVariable Long idRole){
+        try {
+            this.interfaceUtilisateurService.addRoleToUser(idUser, idRole);
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorEntity("201", "Role ajoute avec success"));
+        }catch (CashChangeAppException exception){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorEntity("510", exception.getMessage()));
+        }
+    }
+    @PostMapping("/moveRoleToUserr/{idUser}/{idRole}")
+    public ResponseEntity moveRoleToUser(@PathVariable Long idUser, @PathVariable Long idRole){
+        try {
+            this.interfaceUtilisateurService.moveRoleToUser(idUser, idRole);
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorEntity("201", "Role retire avec success"));
+        }catch (CashChangeAppException exception){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorEntity("510", exception.getMessage()));
+        }
+    }
+    @GetMapping("/loaderUserByEmailUser/{email}")
+    public ResponseEntity loaderUserByEmailUser(@PathVariable String email){
+        try {
+            Utilisateur utilisateur=this.interfaceUtilisateurService.loaderUserByEmailUser(email);
+            return ResponseEntity.ok(utilisateur);
+        }catch (CashChangeAppException exception){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ErrorEntity("510", exception.getMessage()));
+        }
+    }
 }
